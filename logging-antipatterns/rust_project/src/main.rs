9d9fc7115151cee91b7879e3a59ee311c03fbdf5
Hendrik Sollich <hendrik@hoodie.de>
2023-06-16 17:45:03 +0200

    WIP

--- logging-antipatterns/rust_project/src/main.rs
+++ logging-antipatterns/rust_project/src/main.rs
@@ -8,47 +8,57 @@ fn main() {
     let user_id = "honeybear1998";
     let session_id = "D321D003-0083-484A-98F4-9153CD7409E0";
 
-    println!("basic unstructured logs");
+    fn head(title: &str) {
+        eprintln!(r#"<section data-transition="none">"#);
+        eprintln!("<h4>{title}</h4>");
+        eprintln!(r#"<pre><code class="rust-log small">"#);
+    }
+
+    fn food() {
+        eprintln!("</code></pre>");
+        eprintln!("</section>");
+        eprintln!("\n\n");
+    }
+
+    head("basic unstructured logs");
     log::debug!(target: "backend_service::service", "everything fine");
     log::info!(target: "backend_service::main", "listening on {ip:?} with https enabled {https_enabled}");
     log::trace!(target: "backend_service::worker", "started {worker_count} workers");
     log::warn!(target: "backend_service::worker::thread", "session disconnected {user_id:?}");
     log::error!(target: "backend_service::worker::thread", "failure occurred");
+    food();
 
-    println!("\n\n");
-    println!("do not inline variables");
+    head("do not inline variables");
     log::debug!(target: "backend_service::service", "everything fine");
     log::info!(target: "backend_service::main", "listening {{https: {https_enabled}, ip:{ip:?}}}");
     log::trace!(target: "backend_service::worker", "started {worker_count} workers");
     log::warn!(target: "backend_service::worker::thread", "session disconnected {user_id:?}");
     log::error!(target: "backend_service::worker::thread", "failure occurred");
+    food();
 
-    println!("\n\n");
-    println!("be specific");
+    head("be specific");
     log::debug!(target: "backend_service::service::init", "config loaded");
     log::debug!(target: "backend_service::service::transport", "address available");
     log::info!(target: "backend_service::main", "listening {{https: {https_enabled}, ip:{ip:?}}}");
     log::trace!(target: "backend_service::worker", "started {worker_count} workers");
     log::warn!(target: "backend_service::worker::thread", "session disconnected {user_id:?}");
     log::error!(target: "backend_service::worker::thread", "failure occurred");
+    food();
 
-    println!("\n\n");
-
-    println!("give context");
+    head("give context");
     log::debug!(target: "backend_service::service::init", "config loaded");
     log::debug!(target: "backend_service::service::transport", "address available");
     log::info!(target: "backend_service::main", "listening {{https: {https_enabled}, ip:{ip:?}}}");
     log::trace!(target: "backend_service::worker", "started {worker_count} workers");
     log::warn!(target: "backend_service::worker::thread", "session disconnected {{user_id:{user_id:?}}}");
     log::error!(target: "backend_service::worker::thread", "failure occurred {{msg: \"failed to parse input\", user_id: {user_id:?}, worker_id: 4}}");
-    println!("\n\n");
+    food();
 
-    println!("do not reveal PII");
+    head("do not reveal PII");
     log::debug!(target: "backend_service::service::init", "config loaded");
     log::debug!(target: "backend_service::service::transport", "address available");
     log::info!(target: "backend_service::main", "listening {{https: {https_enabled}, ip:{ip:?}}}");
     log::warn!(target: "backend_service::worker::thread", "session disconnected {{session_id:{session_id:?}}}");
     log::error!(target: "backend_service::worker::thread", "failure occurred {{msg: \"failed to parse input\", session_id: {session_id:?}, worker_id: 4}}");
-
-    println!("\n\n");
+    food();
 }
